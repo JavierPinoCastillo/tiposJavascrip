774eb7c59205b0c5c28c187727ac50f985b58987
JavierPinoCastillo <javier.pino.castillo[1@gmail.com>
2022-02-21 13:37:09 -0300

    first commit

---
+++ js-types.js
@@ -0,0 +1,14 @@
+var num =1;
+var numFlotante = 1.5;
+var text = "Esta es una variable texto";
+var bool = false; /* true or false*/
+
+// console.log(num);
+// console. log(numFlotante);
+// console.log(text);
+// console.log(bool);
+
+// var estalloviendo = true;
+
+var numeroCambiante;
+console.log(numeroCambiante);
